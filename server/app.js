b8503ed7c9bf7e87621713079474a0be04445a80
Mariohjr94 <mariohjr94@gmail.com>
2025-04-27 23:59:09 -0400

    new job app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,26 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const jwt = require("jsonwebtoken");
+const helmet = require("helmet")
+const cors = require("cors");
+
+require('dotenv').config();
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+app.use(helmet());
 app.use(cors());
 // Logging middleware
 app.use(morgan("dev"));
+
 
 // Body parsing middleware
 app.use(express.json());
