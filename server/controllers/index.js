b613b918e7b24e16aca1e167fcfb89c87b5e79a4
Mariohjr94 <mariohjr94@gmail.com>
2025-05-01 22:13:57 -0400

    creating routes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
-router.use("/recipes", require("../api/recipes"));
-router.use("/categories", require("../api/categories"));
+router.use("/recipes", require("./recipes"));
+router.use("/categories", require("./categories"));
+router.use("/freezer-items", require("./freezerItems"));
+router.use("/freezer-categories", require("./freezerCategories"));
 
 module.exports = router;
