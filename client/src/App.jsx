be45be2858159c07764243228d311b145ab4e6e6
Mariohjr94 <mariohjr94@gmail.com>
2025-05-14 09:35:34 -0400

    setting vite with tailwind now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css"
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
 
   return (
     <>
-      
+      <div className="min-h-screen bg-yellow-200 flex items-center justify-center">
+      <h1 className="text-4xl text-blue-600 font-bold">Tailwind is WORKING ✅</h1>
+    </div>
+    <Register/>;
+    <Login/>;
     </>
   )
 }
