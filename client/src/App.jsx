bbc824aff617a6b3ca0373c5116cbdba026b8535
Mariohjr94 <mariohjr94@gmail.com>
2025-05-15 23:44:16 -0400

    frontend started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import "./index.css"
-import Login from './pages/Login';
-import Register from './pages/Register';
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="min-h-screen bg-yellow-200 flex items-center justify-center">
-      <h1 className="text-4xl text-blue-600 font-bold">Tailwind is WORKING ✅</h1>
+     <div className="min-h-screen bg-yellow-100 flex items-center justify-center">
+      <div className="bg-white p-6 rounded shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-700">Tailwind is Working 🎉</h1>
+        <p className="text-gray-600 mt-2">This is styled text.</p>
+      </div>
     </div>
-    <Register/>;
-    <Login/>;
-    </>
   )
 }
 
