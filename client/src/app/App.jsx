38f592a852f7335e529ae30c81ca4b4d874710cd
Mariohjr94 <mariohjr94@gmail.com>
2025-05-22 00:14:25 -0400

    creating redux state managment and login form

---
+++ client/src/app/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../App.css'
+import LoginRegister from '../features/auth/LoginRegister'
+
+function App() {
+
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  )
+}
+
+export default App
